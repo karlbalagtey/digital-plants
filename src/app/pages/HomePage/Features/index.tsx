7f49a053d365e8ec7b91c2c9f082a3accc357e46
Karl Mark Balagtey <karl.balagtey@gmail.com>
2021-05-02 23:09:51 +0100

    Removed features on homepage

--- src/app/pages/HomePage/Features/index.tsx
+++ src/app/pages/HomePage/Features/index.tsx
@@ -6,17 +6,10 @@ import { SubTitle } from '../components/SubTitle';
 import { P } from '../components/P';
 import { A } from 'app/components/A';
 import { GithubRepoForm } from './GithubRepoForm';
-import { ThemeSwitch } from './ThemeSwitch';
 import { LanguageSwitch } from './LanguageSwitch';
 import { ReactComponent as StateIcon } from './assets/state.svg';
-import { ReactComponent as CSSIcon } from './assets/css.svg';
 import { ReactComponent as INTLIcon } from './assets/intl.svg';
-import { ReactComponent as TSLogo } from './assets/ts.svg';
 import { ReactComponent as RouteIcon } from './assets/route.svg';
-import { ReactComponent as SEOIcon } from './assets/seo.svg';
-import { ReactComponent as InstantFeedbackIcon } from './assets/instant-feedback.svg';
-import { ReactComponent as ScaffoldingIcon } from './assets/scaffolding.svg';
-import { ReactComponent as CodeAnalysisIcon } from './assets/code-analysis.svg';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'app/components/Link';
 import { messages } from '../messages';
@@ -77,67 +70,6 @@ export function Features() {
               </small>
             </P>
             <LanguageSwitch />
-          </Content>
-        </Feature>
-        <Feature>
-          <RouteIcon className="feature-icon" />
-          <Content>
-            <SubTitle>{t(...messages.routingTitle())}</SubTitle>
-            <P>
-              {t(messages.routingDescription())}
-              <br />
-              <small>
-                Go to our{' '}
-                <Link to={process.env.PUBLIC_URL + '/notfound'}>/NotFound</Link>{' '}
-                page to see how routing works
-              </small>
-            </P>
-          </Content>
-        </Feature>
-        <Feature>
-          <InstantFeedbackIcon className="feature-icon" />
-          <Content>
-            <SubTitle>{t(messages.feedbackTitle())}</SubTitle>
-            <P>{t(messages.feedbackDescription())}</P>
-          </Content>
-        </Feature>
-        <Feature>
-          <ScaffoldingIcon className="feature-icon" />
-          <Content>
-            <SubTitle>{t(messages.scaffoldingTitle())}</SubTitle>
-            <P>{t(messages.scaffoldingDescription())}</P>
-          </Content>
-        </Feature>
-        <Feature>
-          <SEOIcon className="feature-icon" />
-          <Content>
-            <SubTitle>SEO</SubTitle>
-            <P>
-              Supports SEO (document head tags management) for search engines
-              that support indexing of JavaScript content.
-            </P>
-          </Content>
-        </Feature>
-        <Feature>
-          <TSLogo className="feature-icon" />
-          <Content>
-            <SubTitle>TypeScript</SubTitle>
-            <P>
-              Typescript is the key to scalability. Build self-documented code,
-              easy-to-debug code and create maintainable large applications and
-              codebases with a highly productive development experience.
-            </P>
-          </Content>
-        </Feature>
-        <Feature>
-          <CodeAnalysisIcon className="feature-icon" />
-          <Content>
-            <SubTitle>Static Code Analysis</SubTitle>
-            <P>
-              Focus on writing new features without worrying about formatting or
-              code quality. With the right editor setup, your code will
-              automatically be formatted and linted as you work.
-            </P>
           </Content>
         </Feature>
       </List>
